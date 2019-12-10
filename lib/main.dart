3c943a80a46f21e25b18f7531918b7ba54a5fd15
EA Rashel <rashel320@gmail.com>
2019-12-11 00:51:53 +0600

    text styling, padding, container

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,77 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(MaterialApp(
+    title: 'EA App',
+    home: HomePage(),
+  ));
 }
 
-class MyApp extends StatelessWidget{
+//class HomePage extends StatelessWidget {
+//  @override
+//  Widget build(BuildContext context) {
+//    return Scaffold(
+//        appBar: AppBar(title: Text('Home')),
+//        body: Padding(
+//            padding: EdgeInsets.all(20.0),
+//            child: Center(
+//              child: Text(
+//                "This is text This is text This is text This is text This is text",
+//                textAlign: TextAlign.justify,
+//                style: TextStyle(
+//                    color: Colors.cyan,
+//                    fontSize: 20,
+//                    fontStyle: FontStyle.italic),
+//              ),
+//            )));
+//  }
+//}
 
+//class HomePage extends StatelessWidget {
+//  @override
+//  Widget build(BuildContext context) {
+//    return Scaffold(
+//        appBar: AppBar(title: Text('Home')),
+//        body: Container(
+//          margin: EdgeInsets.only(left: 20, top: 20),
+//          padding: EdgeInsets.all(10.0),
+//          height: 100,
+//          width: 200,
+//          decoration: BoxDecoration(
+//              color: Colors.amberAccent,
+//              borderRadius: BorderRadius.circular(8.0)),
+//          child: Text("Container Text",
+//              style: TextStyle(color: Colors.white, fontSize: 20)),
+//        ));
+//  }
+//}
+
+class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-
-    return MaterialApp(
-      title: 'EA APP',
-      home: Scaffold(
+    return Scaffold(
         appBar: AppBar(title: Text('Home')),
-        body: Center(
-          child: Text("This is center text"),
-        )
-      ),
-    );
+        body: Row(
+          children: <Widget>[
+            Container(
+              margin: EdgeInsets.only(left: 20, top: 20),
+              padding: EdgeInsets.all(10.0),
+              height: 50,
+              width: 60,
+              decoration: BoxDecoration(
+                  color: Colors.amberAccent,
+                  borderRadius: BorderRadius.circular(8.0)),
+            ),
+            Container(
+              margin: EdgeInsets.only(left: 20, top: 20),
+              padding: EdgeInsets.all(10.0),
+              height: 50,
+              width: 60,
+              decoration: BoxDecoration(
+                  color: Colors.amberAccent,
+                  borderRadius: BorderRadius.circular(8.0)),
+            )
+          ],
+        ));
   }
 }
-
-
